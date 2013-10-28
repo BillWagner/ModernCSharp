a6c1f09a3a26915118202249d3fe1b904abae092
Bill Wagner <bill.w.wagner@outlook.com>
2013-10-28 05:10:00 -0700

    First Demo, ModernVersion

---
+++ ModernCSharp/ModernCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = from row in Enumerable.Range(1, 8)
+                        from index in Enumerable.Range(0, 8)
+                        let column = (char)('a' + index)
+                        select new { row, column };
+
+            board.ForAll(item => Console.WriteLine(item));
+        }
+    }
+    public static class Extensions
+    {
+        public static void ForAll<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T item in sequence)
+                action(item);
+        }
+    }
+}
